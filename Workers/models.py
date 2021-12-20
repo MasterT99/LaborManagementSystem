8e1f377d21bb2ba0f1c5edc63d09327aa63fc82b
MasterT99 <63745561+MasterT99@users.noreply.github.com>
2021-12-20 12:41:49 +0530

    Update models.py

--- Workers/models.py
+++ Workers/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class Worker(models.Model):
     Worker_id = models.BigAutoField
     Worker_name = models.CharField(max_length=60)
     Worker_company = models.CharField(max_length=60)
-    Worker_color = models.CharField(default="White",max_length=20)
-    Worker_work_profile_id = models.CharField(default="fls",max_length=30)
+    Worker_ptype = models.CharField(default="Carpenter", max_length=20)
+    Worker_work_profile_id = models.CharField(default="fls", max_length=30)
     Worker_description = models.CharField(max_length=1500)
     Worker_price = models.IntegerField()
 
