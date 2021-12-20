8f1784b21148d5d16af169925eaea0d561e7c13d
MasterT99 <63745561+MasterT99@users.noreply.github.com>
2021-12-20 12:41:40 +0530

    Update urls.py

--- CustomerHome/urls.py
+++ CustomerHome/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
-from . import views
-from django.conf.urls import url
 from django.conf import settings
-from django.conf.urls.static import static
 from django.conf.urls import include
+from django.conf.urls.static import static
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
     path('', views.index, name="Home"),
