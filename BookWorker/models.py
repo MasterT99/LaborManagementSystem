fa283e61072a9624c67c929159c364193360cc68
MasterT99 <63745561+MasterT99@users.noreply.github.com>
2021-12-20 11:58:42 +0530

    Update models.py

--- BookWorker/models.py
+++ BookWorker/models.py
@@ -1,23 +1,24 @@
 from django.db import models
-from Workers.models import Worker
+
 from CustomerHome.models import Customer
 from Owner.models import Owner
-from Manager.models import Manager
+from Workers.models import Worker
+
 
 # Create your models here.
 class BookWorker(models.Model):
     BookWorker_id = models.BigAutoField
-    BookWorker_Date_of_Booking = models.DateField(blank=True,null=True)
-    BookWorker_Date_of_Return = models.DateField(blank=True,null=True)
+    BookWorker_Date_of_Booking = models.DateField(blank=True, null=True)
+    BookWorker_Date_of_Return = models.DateField(blank=True, null=True)
     Total_days = models.IntegerField()
-    Advance_amount = models.IntegerField(blank=True,null=True)
-    BookWorker_Total_amount = models.IntegerField(blank=True,null=True)
+    Advance_amount = models.IntegerField(blank=True, null=True)
+    BookWorker_Total_amount = models.IntegerField(blank=True, null=True)
     isAvailable = models.BooleanField(default=True)
     isBillPaid = models.BooleanField(default=False)
     Worker_work_profile_id = models.CharField(max_length=30)
     customer_email = models.CharField(max_length=100)
-    request_responded_by = models.CharField(max_length=100,blank=True,null=True)
-    request_status = models.CharField(max_length=30,default="Pending")
+    request_responded_by = models.CharField(max_length=100, blank=True, null=True)
+    request_status = models.CharField(max_length=30, default="Pending")
 
     def __str__(self):
         return self.customer_email + ": " + str(self.Worker_work_profile_id)
