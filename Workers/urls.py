dbb3d79dbd97f6fe9b79be3449b726034003a25f
MasterT99 <63745561+MasterT99@users.noreply.github.com>
2021-12-20 11:59:04 +0530

    Update urls.py

--- Workers/urls.py
+++ Workers/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
-from . import views
-from django.conf.urls import include
 from django.conf import settings
+from django.conf.urls import include
 from django.conf.urls.static import static
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
     path('AddWork/', views.add_worker, name="AddWork"),
