8a8cb92bce687a0bfc7d5a29f5bdb388629470ed
MasterT99 <63745561+MasterT99@users.noreply.github.com>
2021-12-20 11:58:59 +0530

    Update views.py

--- Workers/views.py
+++ Workers/views.py
@@ -1,26 +1,26 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from Workers.models import Worker
-from Owner.models import Owner
 from Manager.models import Manager
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, redirect
+
+from Owner.models import Owner
+from Workers.models import Worker
+
 
 # Create your views here.
 def add_worker(request):
-    Worker_name=request.POST.get('Worker_name','')
-    Worker_company=request.POST.get('Worker_company','')
-    Worker_model=request.POST.get('Worker_model','')
-    Worker_type=request.POST.get('Worker_type','')
-    Worker_fuel=request.POST.get('Worker_fuel','')
-    Worker_No_of_Seats=request.POST.get('Worker_No_of_Seats',0)
-    Worker_color=request.POST.get('Worker_color','')
-    Worker_work_profile_id=request.POST.get('Worker_work_profile_id','test')
-    
-    Worker_added_by=request.session.get('user_email')
+    Worker_name = request.POST.get('Worker_name', '')
+    Worker_company = request.POST.get('Worker_company', '')
+    Worker_model = request.POST.get('Worker_model', '')
+    Worker_type = request.POST.get('Worker_type', '')
+    Worker_skilllevel = request.POST.get('Worker_skilllevel', '')
+    Worker_No_of_skills = request.POST.get('Worker_No_of_skills', 0)
+    Worker_ptype = request.POST.get('Worker_ptype', '')
+    Worker_work_profile_id = request.POST.get('Worker_work_profile_id', 'test')
 
-    isWorkProfileTemped=request.POST.get('isWorkProfileTemped',True)
-    Worker_description=request.POST.get('Worker_description','')
-    Worker_price=request.POST.get('Worker_price','')
+    Worker_added_by = request.session.get('user_email')
+
+    isWorkProfileTemped = request.POST.get('isWorkProfileTemped', True)
+    Worker_description = request.POST.get('Worker_description', '')
+    Worker_price = request.POST.get('Worker_price', '')
     # Worker_image1=request.FILES['Worker_image1']
     # Worker_image2=request.FILES['Worker_image2']
     # Worker_image3=request.FILES['Worker_image3']
@@ -32,22 +32,23 @@ def add_worker(request):
     if result_worker.exists():
         if result_owner.exists():
             Message = "This Work already exist!!"
-            return render(request,'Owner_Add_Worker.html',{'Message':Message})
+            return render(request, 'Owner_Add_Worker.html', {'Message': Message})
         if result_manager.exists():
             Message = "This Work already exist!!"
-            return render(request,'Manager_Add_Worker.html',{'Message':Message})
+            return render(request, 'Manager_Add_Worker.html', {'Message': Message})
     else:
-        worker=Worker(Worker_name=Worker_name,Worker_company=Worker_company,
-        Worker_model=Worker_model,
+        worker = Worker(Worker_name=Worker_name, Worker_company=Worker_company,
+                        Worker_model=Worker_model,
                         Worker_type=Worker_type,
-                        # Worker_fuel=Worker_fuel,
-        Worker_No_of_Seats=Worker_No_of_Seats,Worker_color=Worker_color,
-        Worker_work_profile_id=Worker_work_profile_id,
-        Worker_added_by=Worker_added_by,isWorkProfileTemped=isWorkProfileTemped,Worker_description=Worker_description,
-        Worker_price=Worker_price
+                        # Worker_skilllevel=Worker_skilllevel,
+                        Worker_No_of_skills=Worker_No_of_skills, Worker_ptype=Worker_ptype,
+                        Worker_work_profile_id=Worker_work_profile_id,
+                        Worker_added_by=Worker_added_by, isWorkProfileTemped=isWorkProfileTemped,
+                        Worker_description=Worker_description,
+                        Worker_price=Worker_price
                         # ,Worker_image1=Worker_image1,Worker_image2=Worker_image2,Worker_image3=Worker_image3
                         )
-        
+
         worker.save()
         if result_owner.exists():
             return redirect('/Owner/AllWork')
